fd91acd8b6b101dc50501f55604dfc094be7cd5e
anurag1207 <46060779+anurag1207@users.noreply.github.com>
2018-12-22 20:27:26 +0530

    django

---
+++ basicforms/basicapp/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.core import validators
+
+class FormName(forms.Form):
+    name=forms.CharField()
+    email=forms.EmailField()
+    verify_email=forms.EmailField(label="enter your email agian:")
+    text=forms.CharField(widget=forms.Textarea)
+    
+    def clean(self):
+        all_clean_data=super().clean();
+        email=all_clean_data['email']
+        vmail=all_clean-data['verify_email']
+        
+        if email!=vmail:
+            raise forms.validationError("make sure email match")
+   
+    
+   
+    
---
+++ basicforms/basicapp/views.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from . import forms
+# Create your views here.
+def index(request):
+    return render(request,'basicapp/index.html')
+def form_name_view(request):
+    form=forms.FormName()
+    if request.method=='POST':
+        form=forms.FormName(request.POST)
+        if form.is_valid():
+            print('validATION SUCCESS!')
+            print("NAME:"+form.cleaned_data['name'])
+            print("EMAIL:"+form.cleaned_data['email'])
+            print("TEXT: "+form.cleaned_data['text'])
+    return render(request,'basicapp/form.html',{'form':form})
